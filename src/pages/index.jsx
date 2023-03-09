95106994fc219ffbaf8bedcc8cf8ef0d8926a7fe
Gerardo <gerardojur@gmail.com>
2023-03-09 11:04:40 -0500

    cambiar nombre de la aplicación a engiversity en título

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,5 @@
 const Index = () => {
-  return <h1 className="m-4 text-2xl text-center font-bold">Bienvenido a la aplicación web EngiVersity</h1>;
+  return <h1 className="m-4 text-2xl text-center font-bold">Bienvenido a la aplicación web Engiversity</h1>;
 }
 
 export default Index;
