7dcc5812245f54bc6b6bfd632b4f00704f897f1e
Gerardo <gerardojur@gmail.com>
2023-03-09 10:56:50 -0500

    cambiar nombre de la aplicación a engiversity

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,5 @@
 const Index = () => {
-  return <h1 className="m-4 text-2xl text-center font-bold">Bienvenido a la aplicación web EduTandem</h1>;
+  return <h1 className="m-4 text-2xl text-center font-bold">Bienvenido a la aplicación web EngiVersity</h1>;
 }
 
 export default Index;
